cf001d77fea9aad8feb58d19e445e14b12985490
razibdash <avrorazib@gmail.com>
2025-09-24 23:54:32 +0600

    added Editor's Picks ,ArticleCardPrimary components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,43 @@
+import HomeContentSection from "@/components/section/home/HomeContentSection";
 import Banner from "@/components/ui/Banner";
-import { connectDB } from "@/lib/mongodb";
-import ArticleModel from "@/models/Article";
+import { getHomePageData } from "@/lib/data";
 import Image from "next/image";
 
 export default async function Home () {
-   await connectDB()
-   const article=await ArticleModel.find().lean()
-  //  console.log(article)
+  const {articles}=await getHomePageData()
+   const {editorPicksPrimary, editorPicksSecondary, trendingArticles, sliderArticles, gridArticles, mostRecentArticles, allMostRecentGridArticles, popularArticles} = articles
+   console.log(trendingArticles)
   return (
-    <div>
+    <div >
       <Banner />
+      <div className="blog-container">
+      {
+        editorPicksPrimary && editorPicksSecondary.length> 0 && trendingArticles.length > 0 && (
+          <HomeContentSection
+          editorPicksPrimary={editorPicksPrimary}
+          editorPicksSecondary={editorPicksSecondary}
+          trendingArticles={trendingArticles}
+          />
+        )
+      }
+
+      {
+        // sliderArticles.length > 0 && <FeaturedSliderSection articles={sliderArticles}/>
+      }
+
+      {
+        // gridArticles.length > 0 && <GridAndAds articles={gridArticles}/>
+      } 
+
+      {
+        mostRecentArticles.length > 0 && allMostRecentGridArticles.length > 0 && popularArticles.length > 0 && (
+          // <MostRecentSection mostRecentArticles={mostRecentArticles} allMostRecentGridArticles={allMostRecentGridArticles} popularArticles={popularArticles}/>
+          <p>not</p>
+        )
+      }
+
+
+    </div>
     </div>
   );
 }
---
+++ src/components/section/home/HomeContentSection.tsx
@@ -0,0 +1,71 @@
+'use client'
+import SectionTitle from "@/components/ui/SectionTitle";
+import { IArticle } from "@/models/Article";
+import { FaArrowRight } from "react-icons/fa";
+import ArticleCardPrimary from "./ArticleCardPrimary";
+import Link from "next/link";
+import {motion} from 'framer-motion'
+
+interface HomeContentSectionProps {
+    editorPicksPrimary?: IArticle;
+    editorPicksSecondary: IArticle[];
+    trendingArticles: IArticle[]
+}
+
+const HomeContentSection = ({editorPicksPrimary, editorPicksSecondary, trendingArticles}: HomeContentSectionProps) => {
+  return (
+    <section className='py-12 bg-white text-gray-800'>
+        <div className='flex flex-col md:flex-row -mx-4'>
+            {/* left side */}
+            <div className='w-full md:w-9/12 px-4 mb-10 md:mb-0'>
+                <SectionTitle title="Editor's Picks"/>
+                <div className='flex flex-col md:flex-row -mx-4'>
+                    {/* main featured article */}
+
+                    <motion.div
+                        className="w-full md:w-1/2 px-4 mb-8 md:mb-0"
+                        initial={{ opacity: 0, y: 40 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, ease: "easeOut" }}
+                        >
+                        {editorPicksPrimary && <ArticleCardPrimary article={editorPicksPrimary} />}
+                        
+                        <Link 
+                            href="/archive" 
+                            className="inline-block mt-6 px-6 py-2.5 text-sm uppercase border rounded hover:bg-primary hover:text-white transition"
+                        >
+                            All Featured
+                        </Link>
+                    </motion.div>
+
+                    {/* small featured article*/}
+
+                     {/* <div className='w-full md:w-1/2 px-4 mb-8 md:mb-0'>
+                        {editorPicksSecondary && editorPicksSecondary.map((article, index) => (
+                            <ArticleCardSecondary key={index} article={article}/>
+                        ))}
+                    </div> */}
+
+                </div>
+            </div>
+
+            {/* right side */}
+{/* 
+            <div className='w-full md:w-3/12 px-4'>
+                <SectionTitle title='Trending'/>
+                <ol className='list-none p-0'>
+                    {trendingArticles.map((article, index) => (
+                        <TrendingArticleItem key={index} article={article} index={index}/>
+                    ))}
+                </ol>
+                <Link href="/archive" className='inline-flex items-center text-sm uppercase text-primary hover:underline transition-colors mt-6'>
+                    See all trending
+                    <FaArrowRight className='ml-2' size={12}/>
+                </Link>
+            </div> */}
+        </div>
+    </section>
+  )
+}
+
+export default HomeContentSection
