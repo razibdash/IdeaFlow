3ec0edca510af8ddca60193c922acb7581ae6275
razibdash <avrorazib@gmail.com>
2025-09-24 23:16:33 +0600

    create function for getHomePageData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Banner from "@/components/ui/Banner";
+import { connectDB } from "@/lib/mongodb";
+import ArticleModel from "@/models/Article";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home () {
+   await connectDB()
+   const article=await ArticleModel.find().lean()
+  //  console.log(article)
   return (
     <div>
       <Banner />
